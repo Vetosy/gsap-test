b7b9282e5a4bf2aa71f0723d7fac84697622bac5
Vetosy <xigga11@gmail.com>
2023-08-14 14:19:37 +0300

    added timeline

---
+++ src/js/components/gsaptofrom.js
@@ -0,0 +1,28 @@
+// import gsap from "gsap";
+// const block = document.querySelector('.block')
+
+// gsap.to(block, {
+//   duration: 3,
+//   x: 300,
+//   y: 100,
+//   ease: "bounce.out"
+// });
+
+// gsap.to(block, {
+//   duration: 2,
+//   x: 300,
+// });
+
+// gsap.from(block, {
+//   duration: 2,
+//   x: 300,
+// });
+
+// gsap.fromTo(block, {
+//   width: 0,
+//   height: 0
+// }, {
+//   width: 300,
+//   height: 300,
+//   duration: 2,
+// });
