74a1e74aab9cab3b9fcbf237d8ca9bc90e530bed
Vetosy <xigga11@gmail.com>
2023-08-14 16:04:55 +0300

    added gsap animation text

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,5 @@ import './components/scroll';
 import './components/burger';
 import './components/gsaptofrom';
 import './components/timeline';
+import './components/gsapscroll';
+import './components/gsaptext';
